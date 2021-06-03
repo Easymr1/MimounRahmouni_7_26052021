6c1df049f356d5ff7f94274d71a07ecdba9d8297
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-06-03 09:44:03 -0400

    Création en cours du controller de la table publication

--- database/controllersDB/employesDB.js
+++ database/controllersDB/employesDB.js
@@ -8,12 +8,16 @@ exports.createdb = (req, res) => {
     })
 };
 
-exports.createtable = (req, res) => {
+exports.createtabl_employes = (req, res) => {
     let sql = 'CREATE TABLE employes(id int AUTO_INCREMENT, firstname VARCHAR(50) NOT NULL, lastname VARCHAR(50) NOT NULL, email VARCHAR(200) NOT NULL, password VARCHAR(100) NOT NULL, image_url VARCHAR(200), PRIMARY KEY(id), UNIQUE(email));';
     db.query(sql, err => {
         if (err) {
-            console.log('Table non crée', err)
+            console.log('Table employes non crée', err)
         }
-        res.send('Table Created')
+        res.send('Table Employes Created')
     })
 };
+
+exports.createtabl_employes = (req, res) => {
+
+}
--- database/routesDB/employesDB.js
+++ database/routesDB/employesDB.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const userCtrl = require('../controllersDB/employesDB');
 
 router.post('/createdb', userCtrl.createdb);
-router.post('/createtable', userCtrl.createtable);
+router.post('/createtable', userCtrl.createtabl_employes);
 
 module.exports = router;
