0315cab74c5ab0184bd7ee8a25bd533eec3ac81c
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-06-04 09:32:22 -0400

    Salage signup et verification login reussi

--- controllers/employes.js
+++ controllers/employes.js
@@ -1,39 +1,53 @@
-let db = require('../database/connectMySQL')
+const db = require('../database/connectMySQL')
+const bcrypt = require('bcrypt');
 
 
 
 exports.signup = (req, res, next) => {
-
-    let post = {
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password,
-        image_url: req.body.image_url,
-    };
-
-    let sql = 'INSERT INTO employes SET ?'
-    db.query(sql, post, err => {
-        if (err) {
-            throw res.status(401).json({ error: 'L\'utilisateur existe déja !' });
-        }
-        res.status(201).json({ message: 'Compte employés crée!' });
-    })
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            let post = {
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                password: hash,
+                image_url: req.body.image_url,
+            };
+            let sql = 'INSERT INTO employes SET ?'
+            db.query(sql, post, err => {
+                if (err) {
+                    throw res.status(401).json({ error: 'L\'utilisateur existe déja !' });
+                }
+                res.status(201).json({ message: 'Compte employés crée!' });
+            })
+        })
 };
 
 exports.login = (req, res) => {
-    let sql = `SELECT employes.password FROM employes WHERE email='${req.body.email}'`;
+    let sql = `SELECT * FROM employes WHERE email='${req.body.email}'`;
+
     db.query(sql, (err, results) => {
         if (err) {
             return res.status(400).json({ error: 'Une erreur c\'est produit !' });
-        } else if (results == '') {
-            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-        } else if (req.body.password === results[0].password) {
-            res.send(results)
-        } else {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
         }
-
+        if (results == '') {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        if (req.body.password == undefined) {
+            return res.status(401).json({ error: 'Veiller mettre un mot de passe !' });
+        }
+        bcrypt.compare(req.body.password, results[0].password)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                }
+                res.status(200).json({
+                    employesId: results[0].id,
+                    firstName: results[0].firstname,
+                    lastName: results[0].lastname,
+                    token: 'TOKEN',
+                });
+            })
     })
 }
 
